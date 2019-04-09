7827243c62d647fb9bc9ab77f37067d1df09387e
Ian Cramer <iancramer55@gmail.com>
2019-04-08 21:14:34 -0400

    Added charts

--- src/components/core/Input.js
+++ src/components/core/Input.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
-export const Input = (props) => {
-  let onChange = props.onChange ? (evt) => props.onChange(evt) : null;
-  let onFocus = props.onFocus ? (evt) => props.onFocus(evt) : null;
-  let onBlur = props.onBlur ? (evt) => props.onBlur(evt) : null;
-  let onKeyUp = props.onKeyUp ? (evt) => props.onKeyUp(evt) : null;
-  let onKeyPress = props.onPress ? (evt) => props.onPress(evt) : null;
+export const Input = ({label, ...props}) => {
 
   return (
     <div className="form-group">
-      <label>{props.label || ''}</label>
+      <label>{label || ''}</label>
       <input
         className="form-control"
-        type={props.type || 'text'}
-        onChange={onChange}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        onKeyUp={onKeyUp}
-        onKeyPress={onKeyPress}
+        type="text"
+        {...props}
       />
     </div>
   )
